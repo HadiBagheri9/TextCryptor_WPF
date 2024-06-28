eb30aacc03e7f1f1b42bdc49bd21a37d9105c660
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-06-28 21:22:55 +0330

    ENC/DC develop

---
+++ TextCryptor_WPF/MainClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCryptor_WPF
+{
+    class MainClass
+    {
+        public static string Encrypt(string text, int layers)
+        {
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            string str = Convert.ToBase64String(textBytes);
+            for (int i = 0; i < layers - 1; i++)
+            {
+                textBytes = Encoding.UTF8.GetBytes(str);
+                str = Convert.ToBase64String(textBytes);
+            }
+
+            return str;
+        }
+
+        public static string Decrypt(string text, int layers)
+        {
+            string str = Encoding.UTF8.GetString(Convert.FromBase64String(text));
+
+            for (int i = 0; i < layers -1; i++)
+            {
+                str = Encoding.UTF8.GetString(Convert.FromBase64String(str));
+            }
+
+            return str;
+        }
+    }
+}
--- TextCryptor_WPF/MainWindow.xaml.cs
+++ TextCryptor_WPF/MainWindow.xaml.cs
@@ -27,17 +27,22 @@ namespace TextCryptor_WPF
 
         private void btnEnc_Click(object sender, RoutedEventArgs e)
         {
-
+            string input = new TextRange(txtInput.Document.ContentStart, txtInput.Document.ContentEnd).Text;
+            txtOutput.Document.Blocks.Clear();
+            txtOutput.Document.Blocks.Add(new Paragraph(new Run(MainClass.Encrypt(input, 6))));
         }
 
         private void btnCopy_Click(object sender, RoutedEventArgs e)
         {
-
+            string text = new TextRange(txtOutput.Document.ContentStart, txtOutput.Document.ContentEnd).Text;
+            Clipboard.SetText(text);
         }
 
         private void btnDc_Click(object sender, RoutedEventArgs e)
         {
-
+            string input = new TextRange(txtInput.Document.ContentStart, txtInput.Document.ContentEnd).Text;
+            txtOutput.Document.Blocks.Clear();
+            txtOutput.Document.Blocks.Add(new Paragraph(new Run(MainClass.Decrypt(input, 6))));
         }
     }
 }
